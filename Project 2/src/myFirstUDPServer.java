e45fd8107c06986246605bdb07089866eb9e3f60
jtljrdn <jtlee@rmgj2.com>
2026-03-16 15:57:47 -0500

    update path

--- Project 2/src/myFirstUDPServer.java
+++ Project 2/src/myFirstUDPServer.java
@@ -17,7 +17,7 @@ import java.util.Map;
 public class myFirstUDPServer {
     private static Map<Short, Item> loadCSV() throws IOException {
         Map<Short, Item> catalog = new HashMap<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("src/data.csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("data.csv"))) {
             String line;
             boolean firstLine = true;
             while ((line = br.readLine()) != null) {
